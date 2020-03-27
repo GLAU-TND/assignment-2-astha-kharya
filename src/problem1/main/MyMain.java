fb2c78a6116aca4aebe83d37b113937cf8992a5b
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-27 23:55:09 +0530

    commit main class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,20 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree BinarySearchTree=new MyBinarySearchTree();
+        BinarySearchTree.insert(10);
+        BinarySearchTree.insert(20);
+        BinarySearchTree.insert(30);
+        BinarySearchTree.insert(40);
+        BinarySearchTree.insert(50);
+
+        System.out.println("Left children of binary search tree: ");
+        BinarySearchTree.Traverse();
+        System.out.println("No. of parents not having their child are: ");
+        System.out.println(BinarySearchTree.getCountRight());
+    }
 }
---
+++ src/problem3/Student/student.java
@@ -0,0 +1,11 @@
+package problem3.Student;
+
+public class student {
+    private String name;
+    private int rollno;
+
+    public student(String name, int rollno) {
+        this.name = name;
+        this.rollno = rollno;
+    }
+}
