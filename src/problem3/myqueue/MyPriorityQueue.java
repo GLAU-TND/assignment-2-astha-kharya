0063e088f2b710ae06fdeb07ebc3494460a62689
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 01:02:23 +0530

    make enqueue method

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,13 @@
  */
 package problem3.myqueue;
 
+import problem3.Student.student;
+import problem3.mylinkedlist.MyLinkedList;
+
 public class MyPriorityQueue {
+    MyLinkedList linkedList=new MyLinkedList();
+    public void enqueue(student stu)
+    {
+        linkedList.addLast(stu);
+    }
 }
