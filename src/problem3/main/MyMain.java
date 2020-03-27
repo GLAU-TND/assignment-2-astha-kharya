28cd7d4bd087d6612d2033a612eb1a135f94743d
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 01:17:51 +0530

    commit main class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,22 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.Student.student;
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue =new MyPriorityQueue();
+        queue.enqueue(new student("Astha",15));
+        queue.enqueue(new student("Anshika",10));
+        queue.enqueue(new student("Pari",41));
+        queue.enqueue(new student("Ishika",28));
+        queue.enqueue(new student("Prince",45));
+        queue.show();
+        queue.dequeue();
+        queue.show();
+    }
 }
