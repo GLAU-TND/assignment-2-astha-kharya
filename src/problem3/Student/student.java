65a31f90b4e10c9054e7777b283b713e4df79476
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-27 23:55:56 +0530

    commit student class

--- src/problem3/Student/student.java
+++ src/problem3/Student/student.java
@@ -4,6 +4,22 @@ public class student {
     private String name;
     private int rollno;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
     public student(String name, int rollno) {
         this.name = name;
         this.rollno = rollno;
