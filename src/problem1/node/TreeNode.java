3036ffbf944149394c5797e82c4865c83bc7d2f8
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-27 22:09:05 +0530

    make constructor

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,10 @@ public class TreeNode {
     private int data;
     private TreeNode left;
     private TreeNode right;
+
+    public TreeNode(int data, TreeNode left, TreeNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
 }
