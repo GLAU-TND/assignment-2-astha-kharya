f7b06dc022f0ed02e2c238f5a42b37b138592f06
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 14:56:33 +0530

    create constructor

---
+++ src/problem4/LinkedList/MyLinkedList.java
@@ -0,0 +1,15 @@
+package problem4.LinkedList;
+
+public class MyLinkedList {
+    private Node first;
+    private Node last;
+
+    public void addLast(int data){
+        Node node=new Node(data);
+        if(first==null){
+            first=last=null;
+            return;
+        }
+        last.
+    }
+}
---
+++ src/problem4/LinkedList/Node.java
@@ -0,0 +1,11 @@
+package problem4.LinkedList;
+
+public class Node {
+    private int data;
+    private int Next;
+
+    public Node(int data) {
+        this.data = data;
+        this.Next=Next;
+    }
+}
