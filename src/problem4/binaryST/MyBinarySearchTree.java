8baa1faf538f18fb3dee29308c22af4ba06ad116
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 15:59:27 +0530

    create insert method

--- src/problem4/binaryST/MyBinarySearchTree.java
+++ src/problem4/binaryST/MyBinarySearchTree.java
@@ -1,5 +1,6 @@
 package problem4.binaryST;
 
+import problem1.node.TreeNode;
 import problem4.myqueue.MyQueue;
 
 public class MyBinarySearchTree {
@@ -7,5 +8,31 @@ public class MyBinarySearchTree {
 
     public MyBinarySearchTree(MyQueue myQueue) {
         this.myQueue = myQueue;
+    }
+    public TreeNode root;
+    public void insert(int data){
+        TreeNode node=new TreeNode(data);
+        if(root==null){
+            root=node;
+        }
+        else{
+            TreeNode current=root;
+            TreeNode parent=null;
+            while(current!=null){
+                parent=current;
+                if(node.getData()<current.getData()){
+                    current=current.getLeft();
+                }
+                else {
+                    current=current.getRight();
+                }
+            }
+            if(node.getData()<parent.getData()){
+                parent.setLeft(node);
+            }
+            else{
+                parent.setRight(node);
+            }
+        }
     }
 }
